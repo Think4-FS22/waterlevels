6f67f9aac0898cb2cd71e0812bf60efb025f4e91
chrischne <chrischne@gmail.com>
2022-03-03 14:46:31 +0100

    refine

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 let img;
-let cellsize = 5;
+
+let cellsize = 10;
 let xsteps;
 let ysteps;
 
+let waterlevel = 434;
+
 let slider;
-let minHeight = 397;
-let maxHeight = 3220;
-let waterlevel = 433;
 
 function preload() {
   img = loadImage("heightmapper_397_3220.png");
@@ -16,50 +16,53 @@ function setup() {
   let ratio = img.width / img.height;
   createCanvas(1100, 1100 / ratio);
 
-  console.log(img.width, img.height);
+  slider = createSlider(397, 1000, waterlevel, 1);
+  slider.position(10, 10);
+  slider.changed(updateWaterLevel);
 
   xsteps = width / cellsize;
   ysteps = height / cellsize;
 
-  slider = createSlider(minHeight, maxHeight, waterlevel, 1);
-  slider.position(10, 10);
-
-  slider.changed(updateWaterLevel);
+  noLoop();
 }
 
 function draw() {
-  background(255);
-
-  image(img, 0, 0, width, height);
+  background(250);
 
   for (let i = 0; i < xsteps; i++) {
     for (let j = 0; j < ysteps; j++) {
       let x = i * cellsize;
       let y = j * cellsize;
+
       let lookupX = map(x, 0, width, 0, img.width);
       let lookupY = map(y, 0, height, 0, img.height);
 
       let col = img.get(lookupX, lookupY);
       let r = red(col);
 
-      let level = map(r, 0, 255, 397, 3220);
+      let m = map(r, 0, 255, 397, 3220);
 
-      if (level < waterlevel) {
-        fill(173, 216, 230, 200);
+      if (m < waterlevel) {
+        fill("steelblue");
         noStroke();
-        rect(x, y, cellsize, cellsize);
+        ellipse(x, y, 0.7 * cellsize, 0.7 * cellsize);
+      }
+      if (m >= waterlevel && m < waterlevel + 100) {
+        fill("tomato");
+        noStroke();
+        ellipse(x, y, 0.7 * cellsize, 0.7 * cellsize);
+      }
+      if (m >= waterlevel + 100 && m < waterlevel + 200) {
+        fill("darkseagreen");
+        noStroke();
+        ellipse(x, y, 0.7 * cellsize, 0.7 * cellsize);
       }
     }
   }
-  noStroke();
-  fill(255);
-  text("Water Level: " + waterlevel + " m ü. M.", 20, 50);
-
-  noLoop();
 }
 
 function updateWaterLevel() {
   waterlevel = slider.value();
-  console.log("water level", waterlevel);
+  console.log("updateWaterLevel", waterlevel);
   redraw();
 }
